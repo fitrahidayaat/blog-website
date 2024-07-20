2c3b4212a68fa905d0b3ee46bbe2b5ad695e578c
fitrahidayaat <fitrahidayaat@gmail.com>
2024-07-20 15:07:02 +0700

    fix pagination

--- src/components/blog/Pagination.jsx
+++ src/components/blog/Pagination.jsx
@@ -1,88 +1,206 @@
 import BlogCard from "./BlogCard.jsx";
 // import BLOGS from "../../data/blogs.js";
-import {useState} from "react";
+import { useState } from "react";
 import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 
-
-export default function Pagination({blogs}) {
+export default function Pagination({ blogs }) {
     const [page, setPage] = useState(1);
     const visibleBlogs = blogs.slice((page - 1) * 4, page * 4);
     const totalPage = Math.ceil(blogs.length / 4);
-    const totalPageArray = Array.from({length: totalPage}, (_, index) => index + 1);
+    const totalPageArray = Array.from(
+        { length: totalPage },
+        (_, index) => index + 1
+    );
 
     let paginationButton;
 
     function handlePageChange(newPage) {
-        if(newPage < 1 || newPage > totalPage) return;
-        else if(newPage === '<') setPage(prevPage => prevPage - 1);
-        else if(newPage === '>') setPage(prevPage => prevPage + 1);
+        if (newPage < 1 || newPage > totalPage) return;
+        else if (newPage === "<") setPage((prevPage) => prevPage - 1);
+        else if (newPage === ">") setPage((prevPage) => prevPage + 1);
         else setPage(newPage);
     }
 
     if (totalPage === 1) {
         paginationButton = (
-            <div className="flex justify-center">
-                <button className="">1</button>
-            </div>
+            <>
+                <button
+                    className={`hover:underline ${
+                        page === 1 ? "underline" : ""
+                    }`}
+                >
+                    1
+                </button>
+            </>
         );
     } else if (totalPage > 5) {
-        if(page <= 3){
+        if (page <= 3) {
             paginationButton = (
-                <div className="justify-center flex gap-3">
-                    {
-                        totalPageArray.slice(0, 5).map((_, index) => (
-                            <button onClick={() => handlePageChange(index+1)} key={index}>{index + 1}</button>
-                        ))
-                    }
+                <>
+                    {totalPageArray.slice(0, 5).map((_, index) => (
+                        <button
+                            className={`hover:underline ${
+                                page === index + 1
+                                    ? "underline text-gray-200"
+                                    : ""
+                            }`}
+                            onClick={() => handlePageChange(index + 1)}
+                            key={index}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
                     <p>...</p>
-                    <button onClick={() => handlePageChange(totalPage)}>{totalPage}</button>
-                    <button onClick={() => handlePageChange('>')}>&gt;</button>
-                </div>
+                    <button
+                        className={`hover:underline ${
+                            page === totalPage ? "underline text-gray-200" : ""
+                        }`}
+                        onClick={() => handlePageChange(totalPage)}
+                    >
+                        {totalPage}
+                    </button>
 
-            )
-        } else if(page >= totalPage - 2){
+                    {page < totalPage && (
+                        <button
+                            className={`hover:underline`}
+                            onClick={() => handlePageChange(">")}
+                        >
+                            &gt;
+                        </button>
+                    )}
+                </>
+            );
+        } else if (page >= totalPage - 2) {
             paginationButton = (
-                <div className="justify-center flex gap-3">
-                    <button onClick={() => handlePageChange('<')}>&lt;</button>
-                    <button onClick={() => handlePageChange(1)}>1</button>
-                    <p >...</p>
-                    {
-                        totalPageArray.slice(totalPage - 5).map((_, index) => (
-                            <button onClick={() => handlePageChange(index + totalPage - 5 + 1)} key={index}>{index + totalPage - 5 + 1}</button>
-                        ))
-                    }
-                </div>
-            )
+                <>
+                    {page > 1 && (
+                        <button
+                            className={`hover:underline`}
+                            onClick={() => handlePageChange("<")}
+                        >
+                            &lt;
+                        </button>
+                    )}
+
+                    <button
+                        className={`hover:underline ${
+                            page === 1 ? "underline text-gray-200" : ""
+                        }`}
+                        onClick={() => handlePageChange(1)}
+                    >
+                        1
+                    </button>
+                    <p>...</p>
+                    {totalPageArray.slice(totalPage - 5).map((_, index) => (
+                        <button
+                            className={`hover:underline ${
+                                page === totalPage - 4
+                                    ? "underline text-gray-200"
+                                    : ""
+                            }`}
+                            onClick={() =>
+                                handlePageChange(index + totalPage - 5 + 1)
+                            }
+                            key={index}
+                        >
+                            {index + totalPage - 5 + 1}
+                        </button>
+                    ))}
+                </>
+            );
         } else {
             paginationButton = (
-                <div className="justify-center flex gap-3">
-                    <button onClick={() => handlePageChange('<')}>&lt;</button>
-                    <button onClick={() => handlePageChange(1)}>1</button>
+                <>
+                    {page > 1 && (
+                        <button
+                            className={`hover:underline`}
+                            onClick={() => handlePageChange("<")}
+                        >
+                            &lt;
+                        </button>
+                    )}
+                    <button
+                        className={`hover:underline ${
+                            page === 1 ? "underline text-gray-200" : ""
+                        }`}
+                        onClick={() => handlePageChange(1)}
+                    >
+                        1
+                    </button>
                     <p>...</p>
-                    {
-                        totalPageArray.slice(page - 3, page + 2).map((_, index) => (
-                            <button  onClick={() => handlePageChange(index + page - 3 + 1)} key={index}>{index + page - 3 + 1}</button>
-                        ))
-                    }
+                    {totalPageArray
+                        .slice(page - 3, page + 2)
+                        .map((_, index) => (
+                            <button
+                                className={`hover:underline ${
+                                    page === index + page - 2
+                                        ? "underline text-gray-200"
+                                        : ""
+                                }`}
+                                onClick={() =>
+                                    handlePageChange(index + page - 3 + 1)
+                                }
+                                key={index}
+                            >
+                                {index + page - 3 + 1}
+                            </button>
+                        ))}
                     <p>...</p>
-                    <button onClick={() => handlePageChange(totalPage)}>{totalPage}</button>
-                    <button onClick={() => handlePageChange('>')}>&gt;</button>
-                </div>
-            )
+                    <button
+                        className={`hover:underline ${
+                            page === totalPage ? "underline text-gray-200" : ""
+                        }`}
+                        onClick={() => handlePageChange(totalPage)}
+                    >
+                        {totalPage}
+                    </button>
+
+                    {page < totalPage && (
+                        <button
+                            className={`hover:underline`}
+                            onClick={() => handlePageChange(">")}
+                        >
+                            &gt;
+                        </button>
+                    )}
+                </>
+            );
         }
-    } else if(totalPage > 0) {
+    } else if (totalPage > 0) {
         paginationButton = (
-            <div className="justify-center flex gap-3">
-                <button onClick={() => handlePageChange('<')}>&lt;</button>
-                {
-                    totalPageArray.map((_, index) => (
-                        <button onClick={() => handlePageChange(index+1)} key={index}>{index + 1}</button>
-                    ))
-                }
-                <button onClick={() => handlePageChange('>')}>&gt;</button>
-            </div>
-        )
+            <>
+                {page > 1 && (
+                    <button
+                        className={`hover:underline`}
+                        onClick={() => handlePageChange("<")}
+                    >
+                        &lt;
+                    </button>
+                )}
+
+                {totalPageArray.map((_, index) => (
+                    <button
+                        className={`hover:underline ${
+                            page === index + 1 ? "underline text-gray-200" : ""
+                        }`}
+                        onClick={() => handlePageChange(index + 1)}
+                        key={index}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+
+                {page < totalPage && (
+                    <button
+                        className={`hover:underline`}
+                        onClick={() => handlePageChange(">")}
+                    >
+                        &gt;
+                    </button>
+                )}
+            </>
+        );
     }
 
     const ref = useRef(null);
@@ -101,11 +219,14 @@ export default function Pagination({blogs}) {
             opacity: 1,
             duration: 1,
             ease: "power4.out",
-        })
+        });
     });
 
     return (
-        <div className="py-16 px-10 lg:px-12 xl:px-14 flex flex-col items-center" ref={ref}>
+        <div
+            className="py-16 px-10 lg:px-12 xl:px-14 flex flex-col items-center"
+            ref={ref}
+        >
             <h1 className="text-center text-6xl font-bold">Blog</h1>
             <div className="grid grid-cols-1 xl:grid-cols-2 gap-10 py-24">
                 {visibleBlogs.map((blog, index) => (
@@ -121,7 +242,9 @@ export default function Pagination({blogs}) {
                 ))}
             </div>
 
-            {paginationButton}
+            <div className="justify-center flex gap-10 text-3xl">
+                {paginationButton}
+            </div>
         </div>
     );
 }
